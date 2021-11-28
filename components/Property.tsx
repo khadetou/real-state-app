a28fddbed20fb16d75c8ee45c7f1e92fb44058ea
khadetou <khadetou96@gmail.com>
2021-11-28 17:32:10 +0000

    chakraprovider

--- components/Property.tsx
+++ components/Property.tsx
@@ -21,6 +21,27 @@ const Property = ({ property: { externalID, coverPhoto, price, rentFrequency, ro
                         height={250}
                     />
                 </Box>
+                <Box w="full">
+                    <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                        <Flex alignItems="center">
+                            <Box
+                                color="green.400" paddingRight="3"
+                            >
+                                {isVerified && <GoVerified />}
+                            </Box>
+                            <Text fontSize="lg" fontWeight="bold">AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
+                        </Flex>
+                        <Box >
+                            <Avatar size="sm" src={agency?.logo?.url} />
+                        </Box>
+                    </Flex>
+                    <Flex alignItems="center" justifyContent="space-between" w="250px" color="blue.400">
+                        {rooms}<FaBed /> |  {baths}<FaBath /> | {millify(area)} sqft <BsGridFill />
+                    </Flex>
+                    <Text fontSize='lg'>
+                        {title.length > 30 ? title.substring(0, 30) + '...' : title}
+                    </Text>
+                </Box>
             </Flex>
         </Link>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp
